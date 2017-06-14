2569c51a2a08f9a659e24202c6b8417127b95ea0
messity <meseretgirma44@gmail.com>
2017-06-14 00:16:31 -0700

    Add files via upload

---
+++ sub_menus_browse.php
@@ -0,0 +1,8 @@
+<ul>
+				  <li><a href="uploadvideo.php">Share Videos</a></li>
+				  <li><a href="upload.php">Upload Documents</a></li>
+				  <li><a href="add_pictures.php">Add Image</a></li>
+				  <li><a href="write_articles.php">Posts</a></li>
+                  <li><a href="index.php">Logout</a></li>
+				  
+				</ul>
