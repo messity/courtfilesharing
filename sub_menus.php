f92bf5eb03837b0fa10a69acfa02c8c3479b7712
messity <meseretgirma44@gmail.com>
2017-06-14 00:14:10 -0700

    Add files via upload

---
+++ sub_menus.php
@@ -0,0 +1,7 @@
+<ul>
+			 <li><a href="view_all_resources.php">View All resources</a></li>
+				  <li><a href="search_all_files.php">search file</a></li>
+				  <li><a href="download.php">Download Documents</a></li>
+				  <li><a href="picture.php">Photo Gallery</a></li>
+				  <li><a href="article_records.php">Articles</a></li>
+				</ul>
