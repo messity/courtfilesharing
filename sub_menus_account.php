5997905441bf656fde4751d7920385e71bef7f35
messity <meseretgirma44@gmail.com>
2017-06-14 00:15:07 -0700

    Add files via upload

---
+++ sub_menus_account.php
@@ -0,0 +1,6 @@
+<ul>
+				  <li><b><font color=#afdfa0 face="Comic Sans MS,Lucida Console"><u>My Account Options</u> . . .</font></b></li>
+				  <li><a href="profile.php">View Profile</a></li>
+				  <li><a href="update_profile.php">Update Profile</a></li>
+				  <li><a href="change_password.php">Change Password</a></li>
+				</ul>
